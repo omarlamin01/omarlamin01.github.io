eee98af36286a1090dd85c4e54e6201a8c019d8a
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-06-18 16:11:44 +0000

    working on socials links & email

--- Private/general_setting.php
+++ Private/general_setting.php
@@ -2,3 +2,10 @@
     $title = "My portfolio - Lamine Omar";
     $icon_path = "res\img\my logo - dark.png";
     $logo_path = "res\img\logo.png";
+
+    //Socials links
+    $github = "https://github.com/omarlamin01";
+    $instagram = "https://instagram.com/omarlamin01";
+    $linkedin = "https://linkedin.com/in/https://www.linkedin.com/in/omar-lamine-267333198/";
+    $stackoverflow = "https://stackoverflow.com/users/19355123";
+    $codepen = "";
--- Public/index.php
+++ Public/index.php
@@ -21,7 +21,7 @@ require "import_setting.php";
                     <img src="<?php echo $logo_path; ?>" alt="my logo" height="60px">
                     <span>Lamine Omar</span>
                 </a>
-            </div>    
+            </div>
             <div id="links">
                 <ul>
                     <li><a href="#about">About me</a></li>
@@ -30,37 +30,63 @@ require "import_setting.php";
                 </ul>
                 <button>Resume</button>
                 <i id="toggle"></i>
-            </div>    
+            </div>
         </nav>
     </header>
-    <div class="right">
-        <a href="mailto: contactomarlamine1@gmail.com">contactomarlamine1@gmail.com</a>
+    <div orientation="right" class="right">
+        <div class="email">
+            <a href="mailto: contactomarlamine1@gmail.com">contactomarlamine1@gmail.com</a>
+        </div>
     </div>
-    <div class="left">
-        <ul>
+    <div orientation="left" class="left">
+        <ul class="socials">
             <li>
-                <a href="">
-                    <i></i>
+                <a href="<?php echo $github; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+                        <title>GitHub</title>
+                        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                    </svg>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i></i>
+                <a href="<?php echo $instagram; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+                        <title>Instagram</title>
+                        <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+                    </svg>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i></i>
+                <a href="<?php echo $linkedin; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
+                        <title>LinkedIn</title>
+                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                        <rect x="2" y="9" width="4" height="12"></rect>
+                        <circle cx="4" cy="4" r="2"></circle>
+                    </svg>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i></i>
+                <a href="<?php echo $stackoverflow; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stack-overflow" viewBox="0 0 16 16">
+                        <title>StackOverFlow</title>
+                        <path d="M12.412 14.572V10.29h1.428V16H1v-5.71h1.428v4.282h9.984z" />
+                        <path d="M3.857 13.145h7.137v-1.428H3.857v1.428zM10.254 0 9.108.852l4.26 5.727 1.146-.852L10.254 0zm-3.54 3.377 5.484 4.567.913-1.097L7.627 2.28l-.914 1.097zM4.922 6.55l6.47 3.013.603-1.294-6.47-3.013-.603 1.294zm-.925 3.344 6.985 1.469.294-1.398-6.985-1.468-.294 1.397z" />
+                    </svg>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i></i>
+                <a href="<?php echo $codepen; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-codepen">
+                        <title>CodePen</title>
+                        <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
+                        <line x1="12" y1="22" x2="12" y2="15.5"></line>
+                        <polyline points="22 8.5 12 15.5 2 8.5"></polyline>
+                        <polyline points="2 15.5 12 8.5 22 15.5"></polyline>
+                        <line x1="12" y1="2" x2="12" y2="8.5"></line>
+                    </svg>
                 </a>
             </li>
         </ul>
