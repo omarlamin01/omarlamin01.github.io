e285c21e48d133f3429af5392ef120ce99fa5608
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-06-18 19:19:17 +0000

    finishing the navbar

--- Private/general_setting.php
+++ Private/general_setting.php
@@ -8,4 +8,5 @@
     $instagram = "https://instagram.com/omarlamin01";
     $linkedin = "https://linkedin.com/in/https://www.linkedin.com/in/omar-lamine-267333198/";
     $stackoverflow = "https://stackoverflow.com/users/19355123";
-    $codepen = "";
+    $codepen = "https://codepen.io/omarlamin01";
+    $proemail = "contactomarlamine1@gmail.com";
--- Public/index.php
+++ Public/index.php
@@ -28,20 +28,22 @@ require "import_setting.php";
                     <li><a href="#work">Work</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
-                <button>Resume</button>
-                <i id="toggle"></i>
+                <button>
+                    <title>CV</title>
+                    <a href="res/lamine-omar-cv.pdf">Resume</a>
+                </button>
             </div>
         </nav>
     </header>
     <div orientation="right" class="right">
         <div class="email">
-            <a href="mailto: contactomarlamine1@gmail.com">contactomarlamine1@gmail.com</a>
+            <a href="mailto: <?php echo $proemail; ?>"><?php echo $proemail; ?></a>
         </div>
     </div>
     <div orientation="left" class="left">
         <ul class="socials">
             <li>
-                <a href="<?php echo $github; ?>">
+                <a href="<?php echo $github; ?>" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
                         <title>GitHub</title>
                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
@@ -49,7 +51,7 @@ require "import_setting.php";
                 </a>
             </li>
             <li>
-                <a href="<?php echo $instagram; ?>">
+                <a href="<?php echo $instagram; ?>" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
                         <title>Instagram</title>
                         <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
@@ -59,7 +61,7 @@ require "import_setting.php";
                 </a>
             </li>
             <li>
-                <a href="<?php echo $linkedin; ?>">
+                <a href="<?php echo $linkedin; ?>" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
                         <title>LinkedIn</title>
                         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
@@ -69,7 +71,7 @@ require "import_setting.php";
                 </a>
             </li>
             <li>
-                <a href="<?php echo $stackoverflow; ?>">
+                <a href="<?php echo $stackoverflow; ?>" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stack-overflow" viewBox="0 0 16 16">
                         <title>StackOverFlow</title>
                         <path d="M12.412 14.572V10.29h1.428V16H1v-5.71h1.428v4.282h9.984z" />
@@ -78,7 +80,7 @@ require "import_setting.php";
                 </a>
             </li>
             <li>
-                <a href="<?php echo $codepen; ?>">
+                <a href="<?php echo $codepen; ?>" target="_blank">
                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-codepen">
                         <title>CodePen</title>
                         <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon>
