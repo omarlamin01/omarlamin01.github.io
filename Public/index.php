ccb9483eff5bd309781e5041567585f749c2501f
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-06-18 14:36:33 +0000

    fixing the navbar

--- Public/index.php
+++ Public/index.php
@@ -14,9 +14,57 @@ require "import_setting.php";
 </head>
 
 <body>
-    <header>
-        <h1>Something here</h1>
+    <header id="home">
+        <nav>
+            <div id="logo">
+                <a href="#home">
+                    <img src="<?php echo $logo_path; ?>" alt="my logo" height="60px">
+                    <span>Lamine Omar</span>
+                </a>
+            </div>    
+            <div id="links">
+                <ul>
+                    <li><a href="#about">About me</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <button>Resume</button>
+                <i id="toggle"></i>
+            </div>    
+        </nav>
     </header>
+    <div class="right">
+        <a href="mailto: contactomarlamine1@gmail.com">contactomarlamine1@gmail.com</a>
+    </div>
+    <div class="left">
+        <ul>
+            <li>
+                <a href="">
+                    <i></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i></i>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i></i>
+                </a>
+            </li>
+        </ul>
+    </div>
     <section id="about"></section>
     <section id="work"></section>
     <section id="contact"></section>
