9a2aaa65b90bc32c3bcf6350b0faa7dfdf44e338
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-07-01 20:54:14 +0000

    s

---
+++ Private/DBconnect.php
@@ -0,0 +1,39 @@
+<?php
+    
+$username = "omarlamin01";
+$password = "karati1985";
+$cluster = "cluster0";
+
+if(extension_loaded("mongodb")) {
+
+    try {
+        //create the manager with the default server adress
+        $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+        //the query that will get all the documents
+        $query = new MongoDB\Driver\Query([]);
+
+        //execute the query
+        $cursor = $manager->executeQuery("myportfolio.admin", $query);
+
+        //show each returned document
+        foreach ($cursor as $doc) {
+            echo "connection succeed." . "<br>";
+            var_dump($doc);
+        }
+    } catch(MongoConnectionException $e) {
+        echo "connection failed!"."<br>";
+    }
+
+    /**
+     * 
+     * $client = new MongoDB\Client(
+     *  'mongodb+srv://' . $username . ':' . $password . '@' . $cluster . '.ryfgcqu.mongodb.net/?retryWrites=true&w=majority'
+     * );
+     * 
+     * $db = $client->test;
+     * 
+     */
+    
+}
+    
