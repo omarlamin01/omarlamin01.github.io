bde51a22d2e5b780f76e2466391cd40256e570f9
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-07-17 20:39:49 +0000

    Update DBconnect.php

--- Private/DBconnect.php
+++ Private/DBconnect.php
@@ -1,13 +1,13 @@
 <?php
-    
+
 $username = "omarlamin01";
 $password = "karati1985";
 $cluster = "cluster0";
 
-if(extension_loaded("mongodb")) {
+if (extension_loaded("mongodb")) {
 
     try {
-        //create the manager with the default server adress
+        /*//create the manager with the default server adress
         $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
         //the query that will get all the documents
@@ -16,24 +16,27 @@ if(extension_loaded("mongodb")) {
         //execute the query
         $cursor = $manager->executeQuery("myportfolio.admin", $query);
 
+        echo "connection succeed." . "<br>";
         //show each returned document
         foreach ($cursor as $doc) {
             echo "connection succeed." . "<br>";
             var_dump($doc);
-        }
-    } catch(MongoConnectionException $e) {
-        echo "connection failed!"."<br>";
+        }*/
+        $client = new MongoDB\Client(
+        'mongodb+srv://' . $username . ':' . $password . '@' . $cluster . '.ryfgcqu.mongodb.net/?retryWrites=true&w=majority'
+              );
+
+        $db = $client->test;
+        echo "connection succeed." . "<br>";
+    } catch (MongoConnectionException $e) {
+        echo "connection failed!" . "<br>";
     }
 
     /**
-     * 
-     * $client = new MongoDB\Client(
-     *  'mongodb+srv://' . $username . ':' . $password . '@' . $cluster . '.ryfgcqu.mongodb.net/?retryWrites=true&w=majority'
-     * );
-     * 
-     * $db = $client->test;
-     * 
+     *
+     *
+     *
      */
-    
+
 }
     
